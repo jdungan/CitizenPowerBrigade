0a6879604acee04fc157abc45f4843fc67331d4b
Blixa Morgan <blixa@projectmakeit.com>
2015-02-21 17:42:28 -0600

    re add lineending to app.js

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ var routes = require('./routes/index');
 
 var app = express();
 
-// view engine setup app.set('views', path.join(__dirname, 'views')); app.set('view engine', 'jade');
+// view engine setup 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/img/favicon.jpg'));
