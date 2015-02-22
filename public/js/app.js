25b3df368d4921b25e0613dc05b224e856288e61
jdungan <johnadungan@gmail.com>
2015-02-22 16:32:18 -0600

    add location registration page

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,33 @@
 L.mapbox.accessToken ='pk.eyJ1IjoiamR1bmdhbiIsImEiOiJlOTl6MFpNIn0.-3o5vIOCjkfXd-7ibZrb8A'
 
+
+$("#use_device").click(function(){
+    if ($(this).attr("checked") == "checked"){
+      navigator.geolocation.getCurrentPosition(function(position) {
+        $('input#lat').val(position.coords.latitude)
+        $('input#lng').val(position.coords.longitude)
+      });
+    } else {
+        $('input#lat').val("")
+        $('input#lng').val("")
+    }
+});
+
+$('#save_location').submit(function (e) {
+  e.preventDefault()
+  $form = $(this)
+  ajax_options ={}
+  ajax_options.data = $form.serialize()
+  ajax_options.type = "POST"
+  ajax_options.url = "https://" + window.location.host
+  $.ajax(ajax_options)  
+
+})
+
 jQuery(document).ready(function() {
 
-  L.mapbox.map('map-canvas', 'jdungan.l9b0mcc7')
-
+  if ($("#map-canvas").length >0) {
+    L.mapbox.map('map-canvas', 'jdungan.l9b0mcc7')
+  }
 });
 
--- src/index.coffee
+++ src/index.coffee
@@ -6,6 +6,11 @@ router.get "/", (req, res) ->
   res.render "index",
     title: "Citizens Power Brigade"
 
+router.get "/location", (req, res) ->
+  res.render "location",
+    title: "Citizens Power Brigade"
+
+
 router.use "/unit", unit
 
 module.exports = router
